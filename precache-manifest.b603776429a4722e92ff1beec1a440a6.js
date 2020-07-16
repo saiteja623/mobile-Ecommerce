e58a7617eebf902b975f7a1e5b6b637fa74f1982
saiteja623 <saiteja.balla13@gmail.com>
2020-07-16 16:46:01 +0530

    Updates

---
+++ precache-manifest.b603776429a4722e92ff1beec1a440a6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9f09a4cea82491221cb85fd365c551",
+    "url": "/mobile-Ecommerce/index.html"
+  },
+  {
+    "revision": "4b131686338e9930d861",
+    "url": "/mobile-Ecommerce/static/css/main.2d615bfd.chunk.css"
+  },
+  {
+    "revision": "0e1a78b34c8c6e1cc7dd",
+    "url": "/mobile-Ecommerce/static/js/2.6688719d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mobile-Ecommerce/static/js/2.6688719d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b131686338e9930d861",
+    "url": "/mobile-Ecommerce/static/js/main.6a3115ad.chunk.js"
+  },
+  {
+    "revision": "311c8e9ea3b416cbf06c",
+    "url": "/mobile-Ecommerce/static/js/runtime-main.f61c3f2c.js"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/mobile-Ecommerce/static/media/product-1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/mobile-Ecommerce/static/media/product-2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/mobile-Ecommerce/static/media/product-3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/mobile-Ecommerce/static/media/product-4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/mobile-Ecommerce/static/media/product-5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/mobile-Ecommerce/static/media/product-6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/mobile-Ecommerce/static/media/product-7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/mobile-Ecommerce/static/media/product-8.d390282f.png"
+  }
+]);
